74e208183a02e55ca65a11e46c5850d0ff64fdfb
Karolina Lewińska <lewinskak95@gmail.com>
2021-07-12 14:42:46 +0200

    Update Procedures_Udemy.sql

--- Procedures_Udemy.sql
+++ Procedures_Udemy.sql
@@ -7,6 +7,7 @@ END
 GO
 EXEC select_all_data
 -------------------------------------
+
 GO
 CREATE PROCEDURE select_courses_by_category
 @subject VARCHAR(20)
@@ -17,7 +18,9 @@ END
 GO
 EXEC select_courses_by_category @subject = 'Musical instruments'
 EXEC select_courses_by_category @subject = 'Web Development' 
+
 -------------------------------------
+
 GO
 CREATE PROCEDURE select_courses_by_keyword
 @keyword VARCHAR(50)
@@ -29,7 +32,9 @@ END
 GO
 EXEC select_courses_by_keyword @keyword = 'Photoshop'
 EXEC select_courses_by_keyword @keyword = 'Excel'
+
 -------------------------------------
+
 GO
 CREATE PROCEDURE select_courses_by_price_range
 @min_price INT,
@@ -41,7 +46,9 @@ BEGIN
 END
 GO
 EXEC select_courses_by_price_range @min_price=100, @max_price=120
+
 -------------------------------------
+
 GO
 CREATE PROCEDURE select_courses_by_number_of_lectures
 @min_num INT
@@ -52,7 +59,9 @@ BEGIN
 END
 GO
 EXEC select_courses_by_number_of_lectures  @min_num=200
+
 -------------------------------------
+
 GO
 CREATE PROCEDURE select_courses_by_date_range
 @min_date DATE,
@@ -64,7 +73,9 @@ BEGIN
 END
 GO
 EXEC select_courses_by_date_range  @min_date ='2017-01-01' , @max_date='2021-01-01'
+
 -------------------------------------
+
 GO
 CREATE PROCEDURE select_n_popular
 @rows_num INT
@@ -75,4 +86,3 @@ END
 GO
 EXEC select_n_popular @rows_num=10
 EXEC select_n_popular @rows_num=100
-
